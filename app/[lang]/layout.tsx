ced1f038a0c9c7e84b1aa4b6508e54499aa5da30
Sofia Ford <scfordw@bu.edu>
2026-01-09 16:50:07 -0500

    Creating dictionaries to hold content for different locales and creating lib directory to hold getDictionary function

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 
@@ -12,7 +11,7 @@ export async function generateStaticParams(){
     return [{lang: "en-US"},{lang:"es-419"}];
 }
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
   params,
 }: Readonly<{
@@ -20,7 +19,7 @@ export default async function RootLayout({
   params: { lang: string};
 }>) {
   return (
-    <html lang={(await params).lang}>
+    <html lang={(params).lang}>
       <body>
         {children}
       </body>
